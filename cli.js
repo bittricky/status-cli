2748a09d2c8900f5c0a9f23998e578563dc5b721
Mitul Patel <mtlpatel@protonmail.com>
2018-07-29 23:18:59 -0400

    node-fetch & conditional

--- cli.js
+++ cli.js
@@ -1,36 +1,33 @@
-const chalk = require('chalk');
+#!/usr/bin/env node
+const fetch = require("node-fetch");
 
 const good = "\033[92m✔\033[0m";
 const bad = "\033[91m✘\033[0m";
 
 function showHelp() {
-  let helpMessage = '';
-  process.stdout.write(helpMessage);
+  let message = `
+    Usage:
+	  $ down [file] [url]
+  `;
+  process.stdout.write(message);
   process.exit();
 }
 
 function printStatus(up, site) {
-  let status = (up) ? 'up' : 'down';
-  process.stdout.write(`${site} is ${status}`);
+  let status = (up) ? `up ${good}` : `down ${bad}`;
+  process.stdout.write(`${site} is ${status} \n`);
 }
 
-function _url(site) {
-  fetch(site, { method: 'GET' })
-    .then(res => {
-      if (res.statusCode != 200) {
-        printStatus(false, site);
-      } else {
-        printStatus(true, site);
-      }
-    })
+function getStatus(site) {
+  fetch(`${site}`, { method: 'GET' })
+    .then(res => (res.status !== 200 ? printStatus(false, site) : printStatus(true, site)))
     .catch(err => printStatus(false, site));
 }
 
-if (process.argv == 1 || process.argv[1] === '-h') {
+if (process.argv[2] === undefined || process.argv[2] === '-h') {
   showHelp();
 }
 
-if (process.argv[1].startsWith('http')) {
-  _url(process.argv[1]);
-  process.exit();
+if (process.argv[2].startsWith('http') || process.argv[2].startsWith('https')) {
+  getStatus(process.argv[2]);
 }
